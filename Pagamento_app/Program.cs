a274455fdc3ee92d3424e9f8396a116fc405fe9c
¨lucascstro¨ <lukas.castro@live.com>
2026-06-06 19:40:22 -0300

    feat: app de pagamento consumido broker com seu parametro de fila

--- Pagamento_app/Program.cs
+++ Pagamento_app/Program.cs
@@ -2,8 +2,18 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var ws = new WorkerService.WorkerReciver();
+        Console.WriteLine("Worker started.");
+        await ws.CreateConnectionAsync();
+        Console.WriteLine("Connection started.");
+        
+        Console.WriteLine("Acompanhando fila payment...");
+        while (true)
+        {
+            await ws.StartListeningAsync("payment");
+            await Task.Delay(1000);
+        }
     }
 }

8308fc5a633f65a04f6c00041ed7c7b0f5eb78b0
¨lucascstro¨ <lukas.castro@live.com>
2026-06-06 19:39:55 -0300

    feat: criado worker para consumo do broker

---
+++ WorkerService/WorkerReciver.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+namespace WorkerService;
+
+public class WorkerReciver
+{
+    IConnection connection;
+    IChannel channel;
+
+    public async Task CreateConnectionAsync()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        connection = await factory.CreateConnectionAsync();
+        channel = await connection.CreateChannelAsync();        
+    }
+
+    public async Task StartListeningAsync(string queueName)
+    {
+        var consumer = new AsyncEventingBasicConsumer(channel);
+                
+        consumer.ReceivedAsync += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = System.Text.Encoding.UTF8.GetString(body);
+            Console.WriteLine("Received: {0}", message);
+            return Task.CompletedTask;
+        };
+
+        await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
+    }
+}
